b436832e177f795d1af00e1fa2b868279ef4aad7
Sergio Moreno <sergio.moreno.calzada@everis.com>
2017-05-17 00:43:33 +0200

    .

---
+++ about.bundle.js
@@ -0,0 +1,76 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/common'), require('@angular/router')) :
+	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', '@angular/common', '@angular/router'], factory) :
+	(factory((global.about = global.about || {}),global._angular_core,global._angular_common,global._angular_router));
+}(this, (function (exports,_angular_core,_angular_common,_angular_router) { 'use strict';
+
+var __decorate$2 = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var AboutComponent = (function () {
+    function AboutComponent() {
+    }
+    AboutComponent.prototype.ngOnInit = function () {
+    };
+    return AboutComponent;
+}());
+AboutComponent = __decorate$2([
+    _angular_core.Component({
+        selector: 'app-about',
+        template: "<p>\n  about works!\n</p>\n",
+        styles: [""]
+    }),
+    __metadata("design:paramtypes", [])
+], AboutComponent);
+
+var __decorate$1 = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var routes = [
+    { path: '', component: AboutComponent }
+];
+var AboutRoutingModule = (function () {
+    function AboutRoutingModule() {
+    }
+    return AboutRoutingModule;
+}());
+AboutRoutingModule = __decorate$1([
+    _angular_core.NgModule({
+        imports: [_angular_router.RouterModule.forChild(routes)],
+        exports: [_angular_router.RouterModule]
+    })
+], AboutRoutingModule);
+
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.AboutModule = (function () {
+    function AboutModule() {
+    }
+    return AboutModule;
+}());
+exports.AboutModule = __decorate([
+    _angular_core.NgModule({
+        imports: [
+            _angular_common.CommonModule,
+            AboutRoutingModule
+        ],
+        declarations: [AboutComponent]
+    })
+], exports.AboutModule);
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
